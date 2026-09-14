6786fa88bb86a1c2894e71be79a7e3809eccae8e
axelalfonso320 <axelalfonso321@gmail.com>
2026-09-14 04:23:27 +0000

    Cambio de prueba para el parche

--- src/Saludo.java
+++ src/Saludo.java
@@ -1,6 +1,6 @@
 public class Saludo {
     public static void main(String[] args) {
-        System.out.println("Hola, muy buenas noches, este soy yo modificando el mensaje");
+        System.out.println("ahora ocupamos modificar una parte del archivo para poder realizar el punto 7");
         System.out.println("Este archivo es código fuente Java.");
     }
 }
